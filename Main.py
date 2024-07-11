fb601b7a7d58c6b69f0c22c4a35075aa9976e42f
João Menício <42006233+Menaicimus@users.noreply.github.com>
2024-07-11 14:01:50 +0100

    Add files via upload
    Main do Menício

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # Replace 'COM3' with the appropriate COM port for Arduino and 'COM4' for 3D printer
-arduino_port = 'COM12'
+arduino_port = 'COM8'
 printer_port = 'COM13'
 baud_rate = 115200
 
@@ -14,31 +14,88 @@ print("Arduino OK")
 printer_ser = serial.Serial(printer_port, baud_rate,timeout=2)
 print("Printer OK")
 
-
+# Ligar steppers
 gcode1 = f"M17 \n"
 printer_ser.write(gcode1.encode())
 print("M17 Sent")
 
+# Sistema de coordenadas absoluto
 gcode1 = f"G91 \n"
 printer_ser.write(gcode1.encode())
 print("G91 Sent")
 
+#Homing a todos os eixos
 gcode2 = f"G28 \n"
 printer_ser.write(gcode2.encode())
 print("G28 Sent")
 
-gcode3 = f"G01 Z50 \n"
-printer_ser.write(gcode3.encode())
-print("G28 Sent")
+#Definir feedrate
+gcode3 = f"G01 X0 F200 \n"
+printer_ser.write(gcode2.encode())
+print("feedrate set")
 
-gcode3 = f"G01 X50 Y50 Z70 \n"
-printer_ser.write(gcode3.encode())
-print("G28 Sent")
+x = 0
+y = 0
+z = 0
 
+x_old = x
+y_old = y
+z_old = z
 
 # Function to send a G-code command and receive the response
 def send_gcode(command, printer):
     printer.write(command.encode())
-    time.sleep(0.2)  # Give the printer some time to process the command
+    time.sleep(0.5)  # Give the printer some time to process the command
     response = printer.readline().decode().strip()
     return response
+
+# Traduzir a info recebida do arduino
+def decoder(arduino_data):
+    # Dividir pelas vírgulas
+    parts = arduino_data.split(',')
+
+    # Atribuir coordenadas
+    x = int(parts[0].strip())
+    y = int(parts[1].strip())
+    z = int(parts[2].strip())
+
+    return x, y, z
+
+try:
+    while True:
+        #Ask for information
+        arduino_ser.write("b/T")
+        
+        while arduino_ser.in_waiting == 0:
+            pass
+
+        # Read a line of data from the Arduino
+        arduino_data = arduino_ser.readline().decode('utf-8').strip()
+
+        
+        if arduino_data:
+            print(f'Received from Arduino: {arduino_data}')
+            
+            try:
+                x, y, z = decoder(arduino_data)
+                print(f"Parsed coordinates: X={x}, Y={y}, Z={z}")
+                
+                # Verificar se algo mudou
+                if not x == x_old or not y == y_old or not z == z_old:
+                    send_gcode(f"G01 X{x} Y{y} Z{z} \n")
+                    x_old = x
+                    y_old = y
+                    z_old = z
+                    time.sleep(0.5)
+                
+            except ValueError as e:
+                print(f"Error parsing coordinates: {e}")
+        
+        time.sleep(0.1)  # Adjust the delay as needed
+        
+except serial.SerialException as e:
+    print(f'Serial error: {e}')
+except KeyboardInterrupt:
+    print("Program stopped by User")
+finally:
+    printer_ser.close()
